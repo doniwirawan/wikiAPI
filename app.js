913dffd16551651ee92b9b44c2ce9ef007ee2faf
doniwirawan <doniwirawan166@gmail.com>
2022-08-26 12:41:04 +0800

    fixing

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
+const { urlencoded } = require('body-parser')
+
+// mongoose.connect('', { urlencoded: true })
+
+// const article = new mongoose.Schema({
+//     title: String,
+//     body: String
+// })
+
+// const Article = new mongoose.model('Article', article)
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -15,7 +25,7 @@ app.use(express.static('public'))
 
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, function (err) {
     if (!err) {
         console.log('port running on localhost:3000')
     }
