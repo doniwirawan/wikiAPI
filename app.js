4ea39d2073e195f3c85ef860948b57f295caf9e4
doniwirawan <doniwirawan166@gmail.com>
2022-08-26 13:00:20 +0800

    adding dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
 const { urlencoded } = require('body-parser')
+require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
